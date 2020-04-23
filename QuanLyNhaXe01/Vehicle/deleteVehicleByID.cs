3d7d7cda2da192ef5c66cbc2ff6a72449320fc8e
LuuVanCui <61924545+LuuVanCui@users.noreply.github.com>
2020-04-23 15:49:49 +0700

    Upload

---
+++ QuanLyNhaXe01/Vehicle/deleteVehicleByID.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace QuanLyNhaXe01
+{
+    public partial class deleteVehicleByID : Form
+    {
+        public deleteVehicleByID()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void deleteVehicleByID_Load(object sender, EventArgs e)
+        {
+            comboBoxID.DataSource = vehicle.getVehicle(new SqlCommand("SELECT MaTheXe FROM Xe"));
+            comboBoxID.ValueMember = "MaTheXe";
+            comboBoxID.ValueMember = "MaTheXe";
+            comboBoxID_SelectionChangeCommitted(sender, e);
+        }
+
+        
+    }
+}
